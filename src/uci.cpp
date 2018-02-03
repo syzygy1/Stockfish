ea871fe2341a6ac2d041d40e4d6af795b410a33b
syzygy1 <3028851+syzygy1@users.noreply.github.com>
2018-02-03 21:12:11 +0100

    Fix bench

--- src/uci.cpp
+++ src/uci.cpp
@@ -132,7 +132,7 @@ namespace {
         else if (token == "infinite")  limits.infinite = 1;
         else if (token == "ponder")    ponderMode = true;
 
-    limits.movetime = 100;
+    if (!limits.depth) limits.movetime = 100;
 
     Threads.start_thinking(pos, states, limits, ponderMode);
   }
